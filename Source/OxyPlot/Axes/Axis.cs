8c94866d2df82a20acac43796b79b335783b8af0
acm <acm@htri.net>
2014-11-19 15:56:35 -0600

    Fixed issue where axis showed unnecessary precision

--- Source/OxyPlot/Axes/Axis.cs
+++ Source/OxyPlot/Axes/Axis.cs
@@ -651,20 +651,17 @@ namespace OxyPlot.Axes
                 step *= -1;
             }
 
-            var value = Math.Round(from / step) * step;
+            var startValue = Math.Round(from / step) * step;
             var numberOfValues = Math.Max((int)((to - from) / step), 1);
             var epsilon = step * 1e-3 * Math.Sign(step);
             var values = new List<double>(numberOfValues);
             var i = 0;
+			var lastValue = startValue;
 
-            while (value <= to + epsilon && i < maxTicks)
+            while (lastValue <= to + epsilon && i < maxTicks)
             {
-                i++;
-
-                // try to get rid of numerical noise
-                var v = Math.Round(value / step, 14) * step;
-                values.Add(v);
-                value += step;
+				lastValue = startValue + step * i++;				                
+                values.Add(lastValue);                
             }
 
             return values;
